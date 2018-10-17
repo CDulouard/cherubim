1e7968383db55134acf5d41681b7a5e9380f10d6
CDulouard <clement.dulouard@gmail.com>
2018-10-17 15:46:13 +0200

    ajout de l'index

---
+++ site/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>index</title>
+</head>
+
+<body>
+    	<?php 
+			  	header('Location: Projet_CHERUBIM.php');
+  				exit();
+?>
+</body>
+    
+
+</html>
