051361d75a9ad3dbcc5d7b85bd76d47b745dc8d2
CDulouard <clement.dulouard@gmail.com>
2018-11-03 13:03:51 +0100

    modification de l'interface de modification de mail : quand un des champs n'est pas valide il est re rempli avec la valeur precedement entree.

--- site/user_option/change_mail/change_mail.php
+++ site/user_option/change_mail/change_mail.php
@@ -15,19 +15,56 @@
     </head>
 
     <body>
-    	<?php
-        if(!isset($_POST['mail'])){
+    	<h1>Modifier adresse mail</h1>
+        <?php
+        if(isset($_POST['mail'])){
+            if(preg_match("#^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$#", $_POST['mail'])){
+                if($_POST['mail'] == $_POST['mail_confirm']){
+                    include('\wamp64\www\cherubim\site\ConnectToDB.php');
+                    $req = $bdd->prepare('SELECT password FROM users WHERE username =? ');//on recupere le mot de passe hashe de l utilisateur
+                    $req->execute(array($_SESSION['username']));
+                    $data = $req->fetch();
+                    if(password_verify($_POST['password'], $data['password'])){
+                        $req = $bdd->prepare('UPDATE users SET mail = :mail_new WHERE username = :username'); //on prepare la requete de modification de mail
+                        $req->execute(array( 
+                        'mail_new' => $_POST['mail'], 
+                        'username' => $_SESSION['username']) );
+                        $ok = 1;
+                        echo "<p id = 'info'>Mail modifie avec succes.</p>";
+                        $modif = 1;
+                    }
+                    else{
+                        echo "<p id = 'info'>Mot de passe incorrect.</p>";
+                    }
+                }
+                else{
+                    echo "<p id = 'info'>Les deux adresses mail doivent etre identiques.</p>";
 
+                }
+            }
+            else{
+                echo "<p id = info>Entrez une adresse mail valide.<p>";
+            } 
+        }
+        if(!isset($ok)){
+            $mail = '';
+            if (isset($_POST['mail'])) {
+                $mail = $_POST['mail'];
+            }
+            $mail_confirm = '';
+            if (isset($_POST['mail_confirm'])){
+                $mail_confirm = $_POST['mail_confirm'];
+            }
             echo '
-        <h1>Modifier adresse mail</h1>
+
         <section id = \'formulaire\'>
             <form action="" method="post">
 
                 <p>Nouveau mail : </p>
-                <input type="text" name="mail" placeholder="nouveau mail" autocomplete="mail"/> <br /> <br /> 
+                <input type="text" name="mail" placeholder="nouveau mail" value = "'.$mail.'" autocomplete="mail"/> <br /> <br /> 
 
                 <p>Confirmez mail : </p>
-                <input type="text" name="mail_confirm" placeholder="mail confirm" autocomplete="mail"/> <br /> <br /> 
+                <input type="text" name="mail_confirm" placeholder="mail confirm" value = "'.$mail_confirm.'" autocomplete="mail"/> <br /> <br /> 
     
                 <p>Mot de passe : </p>
                 <input type="password" autocomplete="on" placeholder="password" name="password" /> <br /> <br /> 
@@ -37,36 +74,9 @@
             </form>
 
         </section>';
-        }
-        
-        elseif(preg_match("#^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$#", $_POST['mail'])){
-            if($_POST['mail'] == $_POST['mail_confirm']){
-                include('\wamp64\www\cherubim\site\ConnectToDB.php');
-                $req = $bdd->prepare('SELECT password FROM users WHERE username =? ');//on recupere le mot de passe hashe de l utilisateur
-                $req->execute(array($_SESSION['username']));
-                $data = $req->fetch();
-                if(password_verify($_POST['password'], $data['password'])){
-                    $req = $bdd->prepare('UPDATE users SET mail = :mail_new WHERE username = :username'); //on prepare la requete de modification de mail
-                    $req->execute(array( 
-                    'mail_new' => $_POST['mail'], 
-                    'username' => $_SESSION['username']) );
-                    echo "Mail modifie avec succes.";
-                }
-                else{
-                    echo "Mot de passe incorrect.";
-                }
-            }
-            else{
-                echo "Les deux adresses mail doivent etre identiques.";
-            }
-        echo "<p id = 'navigation'><a href=''>retour</a></br>";
-        }
-        else{
-            echo "Entrez une adresse mail valide.
-            <p id = 'navigation'><a href=''>retour</a></br>";
-        }   
+        }  
         ?>
-
+    <p id = 'navigation'>
         <a href="../../user/interface.php">retour interface utilisateur</a>
         </br>
     	<a href="../../accueil/Projet_CHERUBIM.php">accueil</a>
