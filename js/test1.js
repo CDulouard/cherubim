8dea45daeca4e496b32447c46569b246ddd9faf6
CDulouard <clement.dulouard@gmail.com>
2018-11-03 19:05:00 +0100

    integration du fichier css de l'accueil et reorganisation des dossiers

--- js/test1.js
+++ js/test1.js
@@ -1,4 +1,3 @@
-var nombre = Number(prompt("Entrez un nombre :"));
-if (nombre > 0) {
-    console.log(nombre + " est positif");
+function test(){
+
 }
--- site/accueil/Projet_CHERUBIM.php
+++ site/accueil/Projet_CHERUBIM.php
@@ -3,7 +3,7 @@
 <html>
     <head>
         <meta charset="utf-8" />
-        <link rel="stylesheet" href="../../CSS/style.css" />
+        <link rel="stylesheet" href="../../CSS/accueil.css" />
         <title>Projet CHERUBIM</title>
     </head>
 
@@ -57,7 +57,7 @@
         <main>
         	<article>
         		<h2>Synospis</h2>
-        		<p>AON 2077. La grande guerre est finit depuis 20 ans. Les grandes puissances se sont effondrées et de nouvelles ont pris leur place.</p>
+        		<p><img src="../../img/AON.png" class="imageflottante" alt="Image flottante" />AON 2077. La grande guerre est finit depuis 20 ans. Les grandes puissances se sont effondrées et de nouvelles ont pris leur place.</p>
         		<p>C'est dans la fondation de cette nation que se cache certains secrets sombres d'avant guerre. Bâtie avec les anciennes nations Océaniennes suite à l'effondrement du Commonwealth et sur la volonté d'unification des cultures, cette organisation est devenu l'une des puissances les plus respectées depuis la fin de la guerre.</p>
         		<p>Toutefois, la militarisation extrême des autres puissances força l'Alliance of Oceanian Nation à développer des technologies de combats défensives et de riposte.</p>
         		<p>C'est avec l'arrivé d'une machine oubliée venant d'ailleurs qui va révéler les mystères de sa fondation, ainsi que ceux des horreurs engendrés par la guerre et de ceux qui en sont responsables.</p>
